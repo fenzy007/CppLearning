5d971a52a6e6bda1f6f8a8664b56b8f172486b06
fenzy007 <42529296+fenzy007@users.noreply.github.com>
2023-09-11 03:43:11 -0400

    first c++ program

---
+++ SectionX/ProjectVs1/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "Hello From Project1" << endl;
+
+    return 0;
+}
---
+++ SectionX/ProjectVs2/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "Hello From Project2" << endl;
+
+    int num;
+    cout << "Please enter your favorite number: ";
+    cin >> num;
+
+    cout << "Did you just enter the number " << num << "?" << endl;
+    cout << "Ok! gotcha!!";
+
+    return 0;
+}
