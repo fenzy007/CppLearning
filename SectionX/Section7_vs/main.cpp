39274b21b15f1603bd2e060390ea6a5018305cf5
fenzy007 <42529296+fenzy007@users.noreply.github.com>
2023-09-13 20:51:29 -0400

    reassigning values to arrays

--- SectionX/Section7_vs/main.cpp
+++ SectionX/Section7_vs/main.cpp
@@ -4,11 +4,19 @@ using namespace std;
 
 int main(){
     int test_scores [5] {100, 95, 99, 87, 88};
+    /*
     cout << "First score at index 0: " << test_scores[0] << endl;
     cout << "First score at index 1: " << test_scores[1] << endl;
     cout << "First score at index 2: " << test_scores[2] << endl;
     cout << "First score at index 3: " << test_scores[3] << endl;
-    cout << "First score at index 4: " << test_scores[4] << endl;
+    cout << "First score at index 4: " << test_scores[4] << endl;    
+    */
+
+    cout << "The first score was: " << test_scores[0] << endl;
+
+    test_scores[0] = 120;
+
+    cout << "The new first score is now: " << test_scores[0] << endl;
 
     return 0;
 }
