515c38c6e12a4e046ffe87f50bf1601f77d2f0bd
fenzy007 <42529296+fenzy007@users.noreply.github.com>
2023-09-15 09:40:53 -0400

    euro to dollar converter

--- SectionX/Section8_vs/main.cpp
+++ SectionX/Section8_vs/main.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 int main(){
+    /*
     int num1{100};
     int num2{200};
 
@@ -17,6 +18,25 @@ int main(){
     // cout << num1 << " / " << num2 << " = " << num1 / num2 << endl;
 
     cout << num1 << " % " << num2 << " = " << num1 % num2 << endl;
+
+    */
+
+   //converting Euro to Dollar
+
+   cout << "Welcome to the Euro to Dollar converter" << endl;
+   
+   const double usd_per_eur{1.19};
+
+   cout << "\nEnter the amount of Euro you want to convert: ";
+
+   double euro{0.0};
+   double dollar{0.0};
+
+   cin >> euro;
+   dollar = euro * usd_per_eur;
+
+   cout << "\nThe " << euro << " Euro amount you entered is equivalent to " << dollar << " Dollars." << endl;
+
 
     return 0;
 }
