e9d208d1259d8fc54c8371684dea450a0b6586e2
fenzy007 <42529296+fenzy007@users.noreply.github.com>
2023-09-15 09:12:32 -0400

    section 8 - statements and operators

---
+++ SectionX/Section8_vs/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    int num1{100};
+    int num2{200};
+
+    cout << num1 << " + " << num2 << " = " << num1 + num2 << endl;
+
+    return 0;
+}
