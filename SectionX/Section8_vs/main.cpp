640003bf9228d229257ebf5eb0f205f83798614e
fenzy007 <42529296+fenzy007@users.noreply.github.com>
2023-09-15 09:14:34 -0400

    refined last

--- SectionX/Section8_vs/main.cpp
+++ SectionX/Section8_vs/main.cpp
@@ -7,7 +7,12 @@ int main(){
     int num1{100};
     int num2{200};
 
-    cout << num1 << " + " << num2 << " = " << num1 + num2 << endl;
+    //cout << num1 << " + " << num2 << " = " << num1 + num2 << endl;
+
+    int result{0};
+    result = num1 + num2;
+
+    cout << num1 << " + " << num2 << " = " << result << endl;
 
     return 0;
 }
