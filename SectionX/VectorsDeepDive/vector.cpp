d895ca82099a0f8beb4540415306ccbd7c6ba04d
fenzy007 <42529296+fenzy007@users.noreply.github.com>
2023-09-17 09:37:46 -0400

    Vector Deep Dive - Part I (Modifiers)

---
+++ SectionX/VectorsDeepDive/vector.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    vector <int> n;
+
+    //Assign
+    n.assign(9, 1); // with this we add 9 elements to the vector and each has the value of 1
+
+    //Size wth for loop
+    cout << "The contents are: " << endl;
+    for(int a = 0; a < n.size(); a++)
+    {
+        cout << n[a] << " ";
+        cout << endl;
+    }
+
+    //push back to add element at the end of the vector
+    n.push_back(2);
+    n.push_back(5);
+    int no = n.size();
+    cout << "\nLast Element: " << n[no - 1] << endl;
+
+    cout << "Current vector size: " << n.size() << endl;
+
+    //pop back to remove element at the end of the vector
+    n.pop_back();
+    cout << "\nNow the contents are: "<< endl;
+    for(int a = 0; a < n.size(); a++)
+    {
+        cout << n[a] << " " << endl;
+
+    }
+
+    //insert to add element at a specific position
+    n.insert(n.begin(), 7);
+    cout << "First element is: " << n[0] << endl;
+
+    //clear to delete all elements
+    n.clear();
+    cout << "The last size update is: " << n.size() << endl;
+
+}
