abfbc6808d0f088a0cb557ddbcdc2793324db0c2
fenzy007 <42529296+fenzy007@users.noreply.github.com>
2023-09-28 15:44:25 -0400

    rand function

--- SectionX/Section11_Vs/main.cpp
+++ SectionX/Section11_Vs/main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
-// #include <cstring>
-// #include <cctype>
 #include <cmath>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
 int main(){
+    /*
     double num{};
 
     cout << "Enter a number (double): ";
@@ -27,7 +28,21 @@ int main(){
     cout << "\nEnter a power to raise " << num << " to: ";
     cin >> power;
     cout << num << " raised to the " << power << " power is: " << pow(num, power) << endl;
+    */
 
+    int random_number{};
+    size_t count{10};
+    int min{1};
+    int max{6};
 
+    cout << "Rand_MAX on my system is: " << RAND_MAX << endl;
+    srand(time(nullptr));
+
+    for(size_t i{1}; i <= count; i++){
+        random_number = rand() % max + min;
+        cout << random_number << endl;
+    }
+
+    cout << endl;
     return 0;
 }
