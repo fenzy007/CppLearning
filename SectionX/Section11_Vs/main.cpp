fac9bfd9817d854365b204ea95668b63c40f2315
fenzy007 <42529296+fenzy007@users.noreply.github.com>
2023-09-28 17:04:47 -0400

    calculating the volume of a cylinder

--- SectionX/Section11_Vs/main.cpp
+++ SectionX/Section11_Vs/main.cpp
@@ -13,13 +13,35 @@ double calc_area_circle(double radius){
     return pi * radius * radius;
 }
 
+double calc_volume_cylinder(double radius, double height){
+    // return pi * radius * radius * height;
+    return calc_area_circle(radius) * height;
+}
+
 void area_circle(){
 
     double radius{};
 
     cout << "\nEnter the radius of the circle: ";
     cin >> radius;
-    cout << "The area of a circle with radius " << radius << " is " << calc_area_circle(radius) << endl;
+
+    cout << "The area of a circle with radius " << radius << " is " << 
+    calc_area_circle(radius) << endl;
+}
+
+
+void volume_cylinder(){
+    double radius{};
+    double height{};
+
+    cout << "\nEnter the radius of the circle: ";
+    cin >> radius;
+    cout << "\nEnter the height of the circle: ";
+    cin >> height;
+
+    cout << "The volume of a cylinder with radius" << radius << " and height " << height << " is " <<
+    calc_volume_cylinder(radius, height) << endl;
+
 }
 
 int main(){
@@ -62,7 +84,7 @@ int main(){
     */
 
     area_circle();
-    // volume_cylinder();
+    volume_cylinder();
 
     cout << endl;
     return 0;
