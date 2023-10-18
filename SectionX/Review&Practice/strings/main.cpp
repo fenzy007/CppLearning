71fc879a4b87281ed688a2493c887277f0132565
fenzy007 <42529296+fenzy007@users.noreply.github.com>
2023-10-18 07:24:39 -0400

    c-style string continues

--- SectionX/Review&Practice/strings/main.cpp
+++ SectionX/Review&Practice/strings/main.cpp
@@ -17,8 +17,8 @@ int main()
     
     //How to get full name, other ways
     char first_name[10] {};
-    // char last_name[100] {};
-    // char full_name[100] {};
+    char last_name[100] {};
+    char full_name[100] {};
     
     cout << "Please enter your first name: ";
     cin >> first_name;
@@ -30,6 +30,7 @@ int main()
         }
     }
     */
+    
     for(size_t i = 0; i < strlen(first_name); i++){
         if(isalpha(first_name[i])){
             first_name[i] = toupper(first_name[i]);
@@ -37,9 +38,15 @@ int main()
     }
     cout << "Your first name is " << first_name << "."<< endl;
     
-    /*
+    
     cout << "\nPlease enter your last name: ";
     cin >> last_name;
+
+    for(size_t i = 0; i < strlen(last_name); i++){
+        if(isalpha(last_name[i])){
+            last_name[i] = toupper(last_name[i]);
+        }
+    }
     
     cout << "Your last name is " << last_name << endl;
     
@@ -49,12 +56,12 @@ int main()
     
     cout << "************Geting Full Name*************" << endl;
     
-    for(size_t i = 0; i < 10000000000; i++){
+    for(size_t i = 0; i < 1000000000; i++){
         i;
     }
     
     cout << "\nYour full name is " << full_name << endl;
-    */
+    
     
     return 0;
 }
