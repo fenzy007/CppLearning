779eeb6399d3e62332c4ef1bc4243c29724df572
fenzy007 <42529296+fenzy007@users.noreply.github.com>
2023-10-22 01:36:51 -0400

    convert c++ to c complete

--- SectionX/ReviewPractice/strings2/main.cpp
+++ SectionX/ReviewPractice/strings2/main.cpp
@@ -2,18 +2,6 @@
 #include <string>
 
 using namespace std;
-/*
-//Function Prototype
-void char_trans(string *t_string);
-//Function Definition
-void char_trans(string *t_string){
-    for(size_t i{0}; i < t_string.length(); i++){
-        if(t_string[i] = t_string.at(0){
-
-        }
-    }
-}
-*/
 
 //Function call
 int main(){
@@ -23,13 +11,10 @@ int main(){
     cout << "Please enter your name: ";
     cin >> first_name;
 
-    cout << "\nYour first name is " << first_name << endl;
-
-    // char_trans(&first_name);
-
     //transform cpp string to c-style string
     const char *first_name_prt = first_name.c_str();
-    cout << first_name_prt << endl;
+
+    cout << "So your name is " << first_name_prt << ", right?"<< endl;
 
 
     //Add stars after 3 characters
