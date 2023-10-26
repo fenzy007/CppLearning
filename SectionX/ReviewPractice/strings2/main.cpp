bff05a70879c0f03977ee067f512c9daac8ef780
fenzy007 <42529296+fenzy007@users.noreply.github.com>
2023-10-26 01:15:07 -0400

    string continues

--- SectionX/ReviewPractice/strings2/main.cpp
+++ SectionX/ReviewPractice/strings2/main.cpp
@@ -5,25 +5,31 @@ using namespace std;
 
 //Function call
 int main(){
-    /*
+    
     string first_name;
     
     cout << "Please enter your name: ";
     cin >> first_name;
 
+    first_name[0] = toupper(first_name[0]);
+
+    cout << first_name << endl;
+
+    /*
     //transform cpp string to c-style string
     const char *first_name_prt = first_name.c_str();
 
     cout << "So your name is " << first_name_prt << ", right?"<< endl;
     */
 
-
+    /*
     char c[] = "Hello World2"; // we use the cstring lib in this case
     
     string c2 = c;
     
     cout << c2 << endl;
-
+    */
+    
 
 
     //Add stars after 3 characters
