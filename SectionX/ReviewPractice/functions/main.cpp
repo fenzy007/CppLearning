666073e5946eb262a2f9e2137eb69c52f0bb5e99
fenzy007 <42529296+fenzy007@users.noreply.github.com>
2023-11-03 02:18:53 -0400

    function starts

---
+++ SectionX/ReviewPractice/functions/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+
+using namespace std;
+
+//function prototype
+double add_numbers(double, double);
+
+//function definition
+double add_numbers(double a, double b){
+    return a + b;
+}
+
+int main() {    
+    add_numbers(2, 3);
+
+
+    return 0;
+}
