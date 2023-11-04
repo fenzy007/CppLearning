3559e2a8156822a166d364a4ab2d8b8a5f71a5c5
fenzy007 <42529296+fenzy007@users.noreply.github.com>
2023-11-03 21:49:07 -0400

    basic functions

--- SectionX/ReviewPractice/functions/main.cpp
+++ SectionX/ReviewPractice/functions/main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
-
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
-
+/*
 //function prototype
 double add_numbers(double, double);
 
@@ -16,5 +18,32 @@ int main() {
     cout << adds << endl;
 
 
+    return 0;
+}
+*/
+
+//function prototype
+void area_circle();
+double calc_area_circle(double);
+
+const double pi{3.14159};
+
+//function definition
+double calc_area_circle(double rad){
+    return pi * rad * rad;
+}
+
+void area_circle(){
+    double radius;
+
+    cout << "\nEnter the radius of the circle: ";
+    cin >> radius;
+
+    cout << "The area of a circle with radius " << radius << " is " <<
+    calc_area_circle(radius) << endl;
+}
+
+int main(){
+    area_circle();
     return 0;
 }
