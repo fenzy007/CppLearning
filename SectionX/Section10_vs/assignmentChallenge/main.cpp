1920df97c3cb0fef743a401d891e46e55f835b31
marc antenor <fenzy007@yahoo.fr>
2024-03-02 23:38:54 -0500

    string review

--- SectionX/Section10_vs/assignmentChallenge/main.cpp
+++ SectionX/Section10_vs/assignmentChallenge/main.cpp
@@ -11,20 +11,23 @@ int main()
 
     size_t num_letters = letters.length();
 
-    int position {0};
+    int position{0};
 
     // for each letter in the string
-    for (char c: letters) {
+    for (char c : letters)
+    {
 
         size_t num_spaces = num_letters - position;
-        
-        while (num_spaces > 0) {
+
+        while (num_spaces > 0)
+        {
             cout << " ";
             --num_spaces;
         }
 
         // Display in order up to the current character
-        for (size_t j=0; j < position; j++) {
+        for (size_t j = 0; j < position; j++)
+        {
             cout << letters.at(j);
         }
 
@@ -32,7 +35,8 @@ int main()
         cout << c;
 
         // Display the remaining characters in reverse order
-        for (int j=position-1; j >=0; --j) {
+        for (int j = position - 1; j >= 0; --j)
+        {
             // You can use this line to get rid of the size_t vs int warning if you want
             auto k = static_cast<size_t>(j);
             cout << letters.at(k);
