d3d7ea159af8991594a51cd2eb3147a923fec42e
marc antenor <fenzy007@yahoo.fr>
2024-03-11 01:20:54 -0400

    triangles with numbers

--- SectionX/Section10_vs/main.cpp
+++ SectionX/Section10_vs/main.cpp
@@ -3,63 +3,6 @@
 #include <cctype>
 
 using namespace std;
-void print_pattern(int *n);
-
-void print_pattern(int *n)
-{
-    // print normal triangle
-    /*
-    for (int i = 1; i <= *n; i++)
-    {
-        for (int j = 1; j <= i; j++)
-        {
-            cout << "*";
-        }
-        cout << endl;
-    }
-    */
-
-    // print reverse triangle
-    /*
-    for (size_t i = *n; i >= 1; i--)
-    {
-        for (size_t j = i; j > 0; j--)
-        {
-            cout << "*";
-        }
-        cout << endl;
-    }
-    */
-
-    // print inverse triangle
-    for (size_t i = 1; i <= *n; i++)
-    {
-        for (int k = *n - i; k > 0; k--)
-        {
-            cout << " ";
-        }
-        for (int j = 1; j <= i; j++)
-        {
-            cout << "*";
-        }
-        cout << endl;
-    }
-
-    // print reverse inverse triangle
-    for (int i = *n; i >= 1; i--)
-    {
-        for (int k = *n - i; k > 0; k--)
-        {
-            cout << " ";
-        }
-        for (int j = i; j >= 1; j--)
-        {
-            cout << "*";
-        }
-
-        cout << endl;
-    }
-}
 
 int main()
 {
@@ -151,12 +94,6 @@ int main()
 
     cout << endl;
     */
-
-    int numb;
-    cout << "Please enter the amount of iteration you want: ";
-    cin >> numb;
-
-    print_pattern(&numb);
 
     return 0;
 }
