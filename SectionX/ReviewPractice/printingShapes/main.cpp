6db6166a85e4cd1dc74c8ea5e7453ec607099c25
marc antenor <42529296+fenzy007@users.noreply.github.com>
2024-03-24 01:30:44 -0400

    triangle shape using alphabet

--- SectionX/ReviewPractice/printingShapes/main.cpp
+++ SectionX/ReviewPractice/printingShapes/main.cpp
@@ -77,6 +77,7 @@ void print_pattern(int *n)
     }*/
 
     // reverse triangles with numbers
+    /*
     for (int i = *n; i >= 1; i--)
     {
         for (int k = *n - i; k > 0; k--)
@@ -91,6 +92,19 @@ void print_pattern(int *n)
         // {
         //     cout << j;
         // }
+        cout << endl;
+    }    */
+    
+
+    //Using Alphabets
+    char alphabet = 'a';
+    
+    for(int i = 1; i<= *n; i++){
+        for(int j = 1 ; j <= i; j++){
+            cout << alphabet;
+        }
+        
+        alphabet++;
         cout << endl;
     }
 }
