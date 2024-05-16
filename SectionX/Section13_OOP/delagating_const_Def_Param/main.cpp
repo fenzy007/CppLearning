91609dfb1ed3c0d1e01b65485d8dca49b1c73fa8
marc antenor <42529296+fenzy007@users.noreply.github.com>
2024-05-16 13:37:32 -0400

    const def parameters

--- SectionX/Section13_OOP/delagating_const_Def_Param/main.cpp
+++ SectionX/Section13_OOP/delagating_const_Def_Param/main.cpp
@@ -18,15 +18,25 @@ public:
 };
 
 PLayer::PLayer()
-    : PLayer{"None", 0,0}{}
+    : PLayer{"None", 0,0}{
+        cout << "no args const"<< endl;
+    }
 
 PLayer::PLayer(string name_val)
-    :PLayer{name_val, 0, 0}{}
+    :PLayer{name_val, 0, 0}{
+        cout << "single args const"<< endl;
+    }
 
 PLayer::PLayer(string name_val, int health_val, int xp_val)
-    : name{name_val}, health{health_val}, xp{xp_val}{}
+    : name{name_val}, health{health_val}, xp{xp_val}{
+        cout << "three args const"<< endl;
+    }
 
 int main()
 {
+    PLayer empty;
+    PLayer marc{"Marc"};
+    PLayer villain{"Villain", 100, 55};
+    
     return 0;
 }
