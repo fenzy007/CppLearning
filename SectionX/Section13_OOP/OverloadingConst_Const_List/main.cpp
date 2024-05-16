48113c380b8b4428dc3e12c06388b78e9372f598
marc antenor <42529296+fenzy007@users.noreply.github.com>
2024-05-16 12:05:50 -0400

    n/a

--- SectionX/Section13_OOP/OverloadingConst_Const_List/main.cpp
+++ SectionX/Section13_OOP/OverloadingConst_Const_List/main.cpp
@@ -20,25 +20,25 @@ public:
 };
 
 Player::Player()
-    :name{"None"}, health{0}, xp{0} {
+    : name{"None"}, health{0}, xp{0} {
     }
 
 Player::Player(string name_val)
-    :name{name_val}, health{0}, xp{0} {
+    : name{name_val}, health{0}, xp{0} {
     }
     
 Player::Player(string name_val, int health_val, int xp_val)
-    :name{name_val}, health{health_val}, xp{xp_val} {
+    : name{name_val}, health{health_val}, xp{xp_val} {
     }
 
 
 int main()
 {
-    {
-        Player slayer;
-        Player frank{"Frank"};
-        Player villain{"Villain", 100, 55};
-    }
+    
+    Player slayer;
+    Player frank{"Frank"};
+    Player villain{"Villain", 100, 55};
+    
     
 
     return 0;
