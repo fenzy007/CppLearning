3bdeb8cace7568be5966342a7b4816a2a963c45b
marc antenor <42529296+fenzy007@users.noreply.github.com>
2024-06-16 21:05:54 -0400

    static class complete

---
+++ SectionX/Section13_OOP/static_class/Player.cpp
@@ -0,0 +1,28 @@
+#include "Player.h"
+#include <iostream>
+
+int Player::num_players {0};
+//we initialize static data in the .cpp file
+
+Player::Player(std::string name_val, int health_val, int xp_val)
+    :name{name_val}, health{health_val}, xp{xp_val}
+{
+    ++num_players;
+}
+
+Player::Player(const Player &source)
+ : Player{source.name, source.health, source.xp}{
+
+ }
+//no deep copy because we are not using pointers
+
+Player::~Player()
+{
+    --num_players;
+    cout << "Desctructor called for: " << name << endl;
+}
+
+int Player::get_num_players()
+{
+    return num_players;
+}
