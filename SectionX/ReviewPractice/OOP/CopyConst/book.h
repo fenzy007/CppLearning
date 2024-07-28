0e086e5dc4be00e4eee21bae9c6a424ec8359263
marc antenor <42529296+fenzy007@users.noreply.github.com>
2024-07-27 21:29:17 -0400

    copy const with arr start

---
+++ SectionX/ReviewPractice/OOP/CopyConst/book.h
@@ -0,0 +1,19 @@
+#ifndef _book_h_
+#define _book_h_
+
+#include <string>
+
+class book
+{
+private:
+    std::string Class_Name;
+protected:
+    std::string Class_Author;
+    int NumberOfBooks;
+    std::string* BooksTitels;
+    
+public:
+
+};
+
+#endif
