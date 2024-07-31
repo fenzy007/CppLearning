78d678a6202610b40e7930430fcde1927e6848b5
marc antenor <42529296+fenzy007@users.noreply.github.com>
2024-07-31 00:46:18 -0400

    Move constructor start

---
+++ SectionX/ReviewPractice/OOP/MoveConstructor/main.cpp
@@ -0,0 +1,12 @@
+#include "book.h"
+#include <iostream>
+
+using namespace std;
+
+//In this Example we will create the array in main
+//Contrary to the copy const Example
+
+int main()
+{
+    return 0;
+}
