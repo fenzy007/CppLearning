ca76ae0f581ac3db1783fe75405019a4e90b1f6a
marc antenor <42529296+fenzy007@users.noreply.github.com>
2024-07-31 10:02:50 -0400

    Update book.h

---
+++ SectionX/ReviewPractice/OOP/MoveConstructor/book.h
@@ -0,0 +1,14 @@
+#ifndef _book_h_
+#define _book_h_
+//In this Example we will create the array in main
+//Contrary to the copy const Example
+#include <string>
+
+class BooksToRead
+{
+private:
+protected:
+public:
+};
+
+#endif _book_h_
