b955c380c13aa0ba365431d7ca8c4e445c159f31
marc antenor <42529296+fenzy007@users.noreply.github.com>
2024-08-05 01:54:59 -0400

    vector of object with pointer

---
+++ SectionX/ReviewPractice/OOP/VECTOR_OBJECT/vectOfObject_P2/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class MyClass
+{
+private:
+    int num;
+
+public:
+    explicit MyClass(int x = 0): num{x}{}
+    void print() const { cout << "num= " << num << endl;}
+    void change(int x) {num = x;}
+};
+
+int main()
+{
+    vector<MyClass> vec1;
+    vector<MyClass*> vec2;
+    MyClass varb;
+
+    for(int i=0; i<=5; i++)
+    {
+        varb.change(i);
+        vec1.push_back(varb);
+        vec2.push_back(&varb);
+    }
+
+    cout << "vec1: " << endl;
+    for(MyClass const& v:vec1)
+    {
+        v.print();
+    }
+    cout << endl;
+
+    cout << "vec2: " << endl;
+    for(MyClass* const& v:vec2)
+    {
+        v->print();
+    }
+
+    return 0;
+}
