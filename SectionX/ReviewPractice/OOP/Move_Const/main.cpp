ff57736c93b68afaa29565655809495f358c9c21
marc antenor <42529296+fenzy007@users.noreply.github.com>
2024-08-26 06:48:26 -0400

    move constructor continue

---
+++ SectionX/ReviewPractice/OOP/Move_Const/main.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <cstring>
+#include <cctype>
+
+
+using namespace std;
+
+class STRING
+{
+private:
+ char* m_Data;
+ size_t m_Size;
+
+public:
+    STRING() = default;
+    STRING(const char* user_string)
+    {
+        printf("Created!\n");
+        m_Size = strlen(user_string);
+        m_Data = new char[m_Size];
+        memcpy(m_Data, user_string, m_Size);
+    }
+
+    STRING(const STRING& other)
+    {
+        printf("Copied!\n");
+        m_Size = other.m_Size;
+        m_Data = new char[m_Size];
+        memcpy(m_Data, other.m_Data, m_Size);
+    }
+
+    ~STRING()
+    {
+        delete[]m_Data;
+    }
+
+    void print()
+    {
+        for(size_t i = 0; i < m_Size; i++)
+        {
+            printf("%c", m_Data[i]);
+        }
+        printf("\n");
+    }
+};
+
+class Entity
+{
+private: STRING m_Name;
+
+public:
+    Entity(const STRING& name)
+        : m_Name(name){ }
+    
+    void PrintName()
+    {
+        m_Name.print();
+    }
+};
+
+int main()
+{
+    Entity entity(STRING("Fenzy"));
+
+    return 0;
+}
