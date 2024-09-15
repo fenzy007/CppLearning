3f43a01a30aa46a2c627861b4e10062cec9deda3
marc antenor <42529296+fenzy007@users.noreply.github.com>
2024-09-14 22:35:10 -0400

    basics of copy and move constructor

--- SectionX/ReviewPractice/OOP/Move_Const/pt2/main.cpp
+++ SectionX/ReviewPractice/OOP/Move_Const/pt2/main.cpp
@@ -3,7 +3,7 @@
 #include <string>
 
 using namespace std;
-
+/*
 void printVector(vector<int> &v)
 {
     cout<<"Printed items are: ";
@@ -30,8 +30,62 @@ main()
     printVector(v1);
     printVector(v2);
     //v2 = v1;
-    v2 = move(v1);
+    v2 = move(v1); 
+    //v1 is no longer being printed
+    //AND moved  to v2
 
     printVector(v1);
     printVector(v2);
+}
+*/
+
+//Class Basic Understanding copy and move
+class TestClass
+{
+public:
+
+    TestClass()
+    {
+        cout << "Default Constructor" << endl;
+    }
+
+    TestClass(const TestClass &source)
+    {
+        cout << "Copy Constructor" << endl;
+    }
+
+    TestClass &operator = (const TestClass &source) noexcept
+    {
+        if(&source != this)
+        {
+            cout << "Copy Assigment Operator" << endl;
+            return *this;
+        }
+        
+    }
+
+    TestClass(TestClass &&original)
+    {
+        cout << "Move constructor" << endl;
+    }
+
+    TestClass &operator = (TestClass &&original) noexcept
+    {
+        cout << "Move assignment operator" << endl;
+    }
+
+};
+
+int main()
+{
+    TestClass a;
+    TestClass b(a);
+
+    TestClass c;
+    c = a;
+
+    TestClass d(move(c));
+
+    TestClass e;
+    e = move(b);
 }
