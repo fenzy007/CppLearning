ad888130f7a223795989f2d0a7ffced8ccbee6f6
marc antenor <42529296+fenzy007@users.noreply.github.com>
2024-11-24 10:32:30 -0500

    overloading continues

--- SectionX/Section14_OperatorOverloading/overloading assignment operators/main.cpp
+++ SectionX/Section14_OperatorOverloading/overloading assignment operators/main.cpp
@@ -2,6 +2,9 @@
 
 using namespace std;
 
+//An assigment occurs when an object has already been initialized and tyou want to assign another object to it
+//memberwise assignment means shallow copy
+//The method operator equal; that's how we declare methods that overload operators
 int main()
 {
     cout << "Hello World" << endl;
