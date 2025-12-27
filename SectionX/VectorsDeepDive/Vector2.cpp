1907a9e558f9f86de2f1558d5ed70fe232ae8eb7
marc antenor <42529296+fenzy007@users.noreply.github.com>
2025-12-26 19:51:29 -0500

    dynamic vec

--- SectionX/VectorsDeepDive/Vector2.cpp
+++ SectionX/VectorsDeepDive/Vector2.cpp
@@ -6,16 +6,22 @@ using namespace std;
 int main()
 {
     vector <int> num;
+    int vector_size{};
 
-    for(int i = 1; i <= 5; i++)
+    cout <<"Please enter the size you want: ";
+    cin >> vector_size;
+
+    for(int i = 1; i <= vector_size; i++)
     {
         num.push_back(i);
     }
 
     cout << "Output from the beginning to the end: ";
+
     for(auto a = num.begin(); a != num.end(); a++)
-    cout << *a << " ";
-    cout << "****"<< endl;
+    {
+        cout << *a << " ";
+    }
 
     return 0;
 
