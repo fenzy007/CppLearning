29c018c4f1475468b37f490cdf2a27db64c873ab
marc antenor <42529296+fenzy007@users.noreply.github.com>
2025-12-29 17:00:04 -0500

    vector erase()

--- SectionX/VectorsDeepDive/vector4.cpp
+++ SectionX/VectorsDeepDive/vector4.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-/*
+
 int main()
 {
-	vector <string> name1 {"wilny", "rose", "fani", "marc"};
-    vector <string> name2 {};
+	vector <string> name1 {"wilny", "rose", "fani", "marc", "ken"};
+    /*vector <string> name2 {};
     
     name2.assign(name1.begin() + 1, name1.end() - 1);
     
@@ -19,8 +19,18 @@ int main()
     for(auto name = name1.begin(); name != name1.end(); name ++)
     {
         cout << *name << endl;
+    }*/
+
+    cout << *(name1.begin() + 2) << endl;
+    cout << *(name1.end() - 1) << endl;
+    cout << "*****" << endl;
+
+    name1.erase(name1.begin(), name1.begin()+2);
+
+    for(auto name : name1)
+    {
+        cout << name << endl;
     }
 
     return 0;
 }
-*/
